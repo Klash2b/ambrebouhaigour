b00cef6fbd74e76b5f5bb5adfddb7f02c668b574
Mathieu Mattei <mattei.mathieu2b1@gmail.com>
2021-06-10 20:39:39 +0200

    Revert "Merge branch 'nav' into main"
    This reverts commit 077dca3b05f2ba62fb9bf61b53a31fe76e214965, reversing
    changes made to 07579aef1297c42284bb3a0b957be83b72fccd19.

--- script/script.js
+++ script/script.js
@@ -11,16 +11,3 @@ function show(id_show, id_hide) {
     document.getElementById(id_show).style.opacity = 1;
   }, 50);
 }
-
-$(function(){
-  var cible;
-  $(".nav-link").click(function(e){
-       e.preventDefault();//empêcher le navigateur de suivre le lien du <a> sur lequel tu as cliqué 
-     cible=$('#'+$(this).attr('href').replace('#',''));
-       $('.navbar-nav').find('.nav-link').removeClass('active');//supprime la class active de tout les a.nav-link
-       $(this).addClass('active');//attribuer la classe active à l’élément cliqué
-       console.log('le id cible :'+cible.attr('id')
-           +' est à :'+cible.offset().top+'px du top.');
-       $('html,body').scrollTop(cible.offset().top);
-   });
-});
